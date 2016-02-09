6972cb9b1d10065ee7aaeb2137faac00c4b76896
Deepti Raghavan <deeptir@mit.edu>
2016-02-09 02:32:54 +0000

    Added indicator loss variable back ontop of latest work, set to 0 everytime

--- src/memory.cc
+++ src/memory.cc
@@ -17,6 +17,8 @@ void Memory::packets_received( const vector< Packet > & packets, const unsigned 
       continue;
     }
 
+    _loss_indicator = 0;
+
     const double rtt = x.tick_received - x.tick_sent;
     if ( _last_tick_sent == 0 || _last_tick_received == 0 ) {
       _last_tick_sent = x.tick_sent;
@@ -40,13 +42,13 @@ void Memory::packets_received( const vector< Packet > & packets, const unsigned 
 string Memory::str( void ) const
 {
   char tmp[ 256 ];
-  snprintf( tmp, 256, "sewma=%f, rewma=%f, rttr=%f, slowrewma=%f", _rec_send_ewma, _rec_rec_ewma, _rtt_ratio, _slow_rec_rec_ewma );
+  snprintf( tmp, 256, "sewma=%f, rewma=%f, rttr=%f, slowrewma=%f, loss_indicator=%f", _rec_send_ewma, _rec_rec_ewma, _rtt_ratio, _slow_rec_rec_ewma, _loss_indicator );
   return tmp;
 }
 
 const Memory & MAX_MEMORY( void )
 {
-  static const Memory max_memory( { 163840, 163840, 163840, 163840 } );
+  static const Memory max_memory( { 163840, 163840, 163840, 163840, 163840 } );
   return max_memory;
 }
 
@@ -57,6 +59,7 @@ RemyBuffers::Memory Memory::DNA( void ) const
   ret.set_rec_rec_ewma( _rec_rec_ewma );
   ret.set_rtt_ratio( _rtt_ratio );
   ret.set_slow_rec_rec_ewma( _slow_rec_rec_ewma );
+  ret.set_loss_indicator( _loss_indicator );
   return ret;
 }
 
@@ -69,6 +72,7 @@ Memory::Memory( const bool is_lower_limit, const RemyBuffers::Memory & dna )
     _rec_rec_ewma( get_val_or_default( dna, rec_rec_ewma, is_lower_limit ) ),
     _rtt_ratio( get_val_or_default( dna, rtt_ratio, is_lower_limit ) ),
     _slow_rec_rec_ewma( get_val_or_default( dna, slow_rec_rec_ewma, is_lower_limit ) ),
+    _loss_indicator ( get_val_or_default( dna, loss_indicator, is_lower_limit ) ),
     _last_tick_sent( 0 ),
     _last_tick_received( 0 ),
     _min_rtt( 0 )
@@ -82,6 +86,7 @@ size_t hash_value( const Memory & mem )
   boost::hash_combine( seed, mem._rec_rec_ewma );
   boost::hash_combine( seed, mem._rtt_ratio );
   boost::hash_combine( seed, mem._slow_rec_rec_ewma );
+  boost::hash_combine( seed, mem._loss_indicator );
 
   return seed;
 }
