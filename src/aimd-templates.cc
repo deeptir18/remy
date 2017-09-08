db780035b1069f918ff6069dc870f0ebd8175e21
Deepti Raghavan <deeptir@mit.edu>
2017-09-07 23:55:09 -0400

    Changes for training cross traffic friendly remy

--- src/aimd-templates.cc
+++ src/aimd-templates.cc
@@ -12,7 +12,6 @@ void Aimd::send( const unsigned int id, NextHop & next, const double & tickno )
 {
   assert( int(_packets_sent) >= _largest_ack + 1 );
   while ( int(_packets_sent) < _largest_ack + 1 + _the_window ) {
-    cout << "In aimd send" << endl;
     Packet p( id, _flow_id, tickno, _packets_sent );
     _packets_sent++;
     next.accept( p, tickno );
